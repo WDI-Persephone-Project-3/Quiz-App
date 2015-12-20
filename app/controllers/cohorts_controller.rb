362cb00daf05dd29167bffe635ebb374690eb6cd
De Olagundoye <de.olagundoye@gmail.com>
2015-12-20 12:13:11 -0500

    back up in cohorts

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,24 +1,25 @@
 class CohortsController < ApplicationController
 	
 	def index
-
+		@instructor = Instructor.find(params[:id])
+		@cohorts = Cohort.find_by(instructor_id params[@instructor])
 	end
 
 	def show
-		@corhort = Cohort.all
-	end
-
-	def show
-		@corhort = 
+		@instructor = Instructor.find(params[:id])
+		@cohorts = Cohort.find_by(instructor_id params[@instructor])
 	end
 
 	def new 
+		@cohorts = Cohort.new
 	end
 
 	def edit
+
 	end
 
 	def create
+		@cohort = Cohort.create(name params[:name], instructor_id)
 	end
 
 	def destroy
