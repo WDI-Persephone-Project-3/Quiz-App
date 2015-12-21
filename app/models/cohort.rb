8ee9ff3feb372cead9b1c07cfed317df48dc2360
Anusha Gandikota <nush1113@gmail.com>
2015-12-20 22:13:52 -0500

    adds mode and median methods

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -10,4 +10,22 @@ class Cohort < ActiveRecord::Base
     students_avg_total = all_students_avg.reduce(:+)
     cohort_avg = students_avg_total.to_f / students.length.to_f
   end
+
+  def cohort_mode
+  	students = self.students
+  	all_students_avg = students.map do |student|
+      student.current_average
+    end
+    all_students_avg.group_by(&:itself).values.max_by(&:size).first #method to find mode found on stackoverflow
+  end
+
+  def cohort_median
+  	students = self.students
+    all_students_avg = students.map do |student|
+      student.current_average
+    end
+    middle = all_students_avg.length / 2
+    median = all_students_avg[middle]
+  end
+
 end
