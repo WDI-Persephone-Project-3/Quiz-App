e56b1d5e9f9dd95345bfa081e7a632568334aebd
Ona Boseroy <o.boseroy@gmail.com>
2015-12-21 10:57:14 -0500

    puts mailer functions in students controllers. deletes user files since emails are sent specifically to students

---
+++ app/mailers/student_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  default from: '<%= instructor.email %>'
+  layout 'mailer'
+
+  def welcome_email(student)
+    @student = student
+    @url = '/'
+    mail(to: @student.email, subject: 'Welcome to Quizapp')
+  end
+
+  def quiz_results_email(student)
+    @student = student
+    @url = '/students/dash'
+    mail(to: @student)
+end
--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'notification@example.com'
-
-  def welcome_email(user)
-    @user = user
-    @url = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to Quizapp')
-  end
-end
