71e02a95afa7a5554165a59f55f44a6c7c8714ce
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-22 09:23:50 -0500

    trying to fix actionMailer fuck up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,21 +14,21 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_deliveries = true
-  # config.action_mailer.default_options = {from: 'instructor@quizapp.com'}
+  # config.action_mailer.perform_deliveries = true
+  # # config.action_mailer.default_options = {from: 'instructor@quizapp.com'}
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-     :address => "smtp.gmail.com",
-     :port => 587,
-     # create a gmail account for this app to use. go into https://myaccount.google.com/ -> sign in & security, -> connected apps & sites -> turn on Allow less secure app
-     :user_name => "ona.quizapp@gmail.com",
-     :password => "p@55w0rd12",
-     :authentication => :plain,
-     :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #    :address => "smtp.gmail.com",
+  #    :port => 587,
+  #    # create a gmail account for this app to use. go into https://myaccount.google.com/ -> sign in & security, -> connected apps & sites -> turn on Allow less secure app
+  #    :user_name => "ona.quizapp@gmail.com",
+  #    :password => "p@55w0rd12",
+  #    :authentication => :plain,
+  #    :enable_starttls_auto => true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
