6f75b91fcc89ea7305d19b1bf96f32d52097ac29
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-25 18:46:14 -0500

    add scroll to all pages to fix issue with resized content, clean up duplicative code, add conditionals so only correct users can see pages & edit content

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,6 +4,7 @@ class ResponsesController < ApplicationController
     @quiz = Quiz.find(params[:id])
     i = 1
 
+    # Questions have the params Q-number and choices have the params number. This weird 2*i logic accounts that for every question, there is a choice param and a question param. The extra 5 params are unimportant to choice selection
     while (2*i)<(params.length-5)
       question_id = params["Q"+i.to_s]
       Response.create(choice: params[question_id], student_id: current_user.id, quiz_id: params[:id], question_id: question_id)
